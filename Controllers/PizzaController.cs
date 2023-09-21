b4b641a2222b86cbf1ce7c8a77ac3fae742d2d6b
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-21 12:18:16 +0530

    PP

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PPWebApp.Models;
+
+namespace PPWebApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        static List<Pizza> pizzas = new List<Pizza>
+        {
+            new Pizza() { Id = 1, Name = "Margherita",Price = 10999 },
+            new Pizza() { Id = 2, Name = "Pepperoni", Price = 12999 },
+            new Pizza() { Id = 3, Name = "Vegetarian", Price = 11999 }
+        };
+        public IActionResult Index()
+        {
+            return View(pizzas);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Pizza());
+        }
+        [HttpPost]
+        public IActionResult Create(Pizza model)
+        {
+            if (ModelState.IsValid)
+            {
+                pizzas.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PPWebApp.Models
+{
+    public class Pizza
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
